57b6152afc701d7a915adf4695079762ebd3447d
ruk <ruk.lakhani@students.makeschool.com>
2019-12-08 13:37:08 -0800

    done

--- app.py
+++ app.py
@@ -71,4 +71,4 @@ def clowns_delete(clown_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
